eefad0bbc5641d2a48487972d8966f267aee9c4e
tgendron <tay.gendron@gmail.com>
2013-10-22 19:49:51 -0400

    ex2

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+a = 55
+b = 0.2
+c = a * b
+
+puts "Your bill is #{a}."
+puts "Your tip pourcentage is #{b}."
+puts "Your till will be #{a * b}. and your total is #{c + a}" 
+
+
+puts "Enter bill price"
+twenty = gets.chomp
+puts "Your tip is #{twenty}"
+
+
+apples = 4
+puts "I have #{apples} apples"
+# or
+puts "I have %s apples" % apples
