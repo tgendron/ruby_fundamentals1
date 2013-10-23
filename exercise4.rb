a58170d309fb8da6d79b9426c71bccdbafe3903d
tgendron <tay.gendron@gmail.com>
2013-10-23 08:42:07 -0400

    ex4 for now

--- exercise4.rb
+++ exercise4.rb
@@ -1,7 +1,19 @@
-one_to_ten = (1..100)
+#wrong
 
+one_to_ten = (1..100)
 one_to_ten.each do |num|
-  print (num).to_s + " "
-  print num = "Bit" if num % 3 == 0
-  print num = "Maker" if num % 5 == 0
+  print (num).to_s + " \n"
+  print "Bit" if num % 3 == 0
+  print "Maker" if num % 5 == 0
+  #print "BitMaker" if num % 3 = num % 5
 end
+
+#right..
+
+puts "\n\n"
+
+   puts (1..100).map {|i|
+  f = i % 3 == 0 ? 'Bit' : nil
+  b = i % 5 == 0 ? 'Maker' : nil
+  f || b ? "#{ f }#{ b }" : i
+  }
