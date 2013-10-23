d175e4e800a0ed63d9e3168bd11b005548de595c
tgendron <tay.gendron@gmail.com>
2013-10-23 01:04:09 -0400

    all good

--- exercise2.rb
+++ exercise2.rb
@@ -1,10 +1,10 @@
 puts "Enter bill $"
 bill = gets.chomp.to_i
 puts "What % of tip would you like to leave?"
-tip = gets.chomp.to_f / 100 
+tip = gets.chomp.to_i
 
-   if tip >= 0.1
-    puts "Thank you, You are so generous! On your #{bill}$ bill you left #{bill * tip}$" 
+   if tip >= 10
+    puts "Thank you, You are so generous! On your #{bill}$ bill you left a #{bill * tip / 100}$ tip" 
   else
     puts "Get out of my restaurant cheapskate!!!"
 end
--- exercise3.rb
+++ exercise3.rb
@@ -1,12 +1,2 @@
-puts "Enter bill $"
-bill = gets.chomp
-puts "What % of tip would you like to leave?"
-tip = gets.chomp
-
-  if tip.to_i >= 10
-    puts "Thank you, You are so generous! On your #{bill}$ bill you left #{bill.to_i * tip.to_i / 100}$" 
-  else
-    puts "Get out of my restaurant cheapskate!!!"
-end
 
 
