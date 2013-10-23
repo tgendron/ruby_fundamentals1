6bbefe4d0d799c08b49e107453505c0a9db7612a
tgendron <tay.gendron@gmail.com>
2013-10-23 00:26:16 -0400

    update

--- exercise2.rb
+++ exercise2.rb
@@ -1,10 +1,10 @@
 puts "Enter bill $"
-bill = gets.chomp
+bill = gets.chomp.to_i
 puts "What % of tip would you like to leave?"
-tip = gets.chomp
+tip = gets.chomp.to_f / 100 
 
-  if tip.to_i >= 10
-    puts "Thank you, You are so generous! On your #{bill}$ bill you left #{bill.to_i * tip.to_i / 100}$" 
+   if tip >= 0.1
+    puts "Thank you, You are so generous! On your #{bill}$ bill you left #{bill * tip}$" 
   else
     puts "Get out of my restaurant cheapskate!!!"
 end
